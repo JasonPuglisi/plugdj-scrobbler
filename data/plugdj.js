8215a7c791172e4bd870f45dad8fe03b5248303b
Jason Puglisi <jason@puglisi.im>
2014-10-26 18:16:18 -0400

    Added scrobbling, icon blue when track is scrobbled

--- data/plugdj.js
+++ data/plugdj.js
@@ -2,51 +2,106 @@ var artist = '';
 var track = '';
 var duration = 0;
 
+var unixStart = 0;
+var start = 0;
+var elapsed = 0;
+var scrobbled = false;
+
+var selfPlaying = 0;
+
 var lastfmSession;
 
 self.port.on('setSession', function(session) {
 	lastfmSession = session;
 })
 
-check();
+checkMedia();
 
-function check() {
-	if (lastfmSession && unsafeWindow.API && unsafeWindow.API.getVolume()) {
-		var API = unsafeWindow.API;
+var checkMediaLoop;
 
-		var media = API.getMedia();
+function checkMedia(response) {
+	if (response) {
+		console.log('Scrobbled: ', response);
+	}
 
-		if (media && media.author && media.title && media.duration) {
-			if (artist !== media.author || track !== media.title || duration !== media.duration) {
-				artist = media.author;
-				track = media.title;
-				duration = media.duration;
+	else {
+		if (lastfmSession && unsafeWindow.API && unsafeWindow.API.getVolume()) {
+			var media = unsafeWindow.API.getMedia();
 
-				process();
+			if (media && media.author && media.title && media.duration) {
+				if (artist !== media.author || track !== media.title || duration !== media.duration) {
+					artist = media.author;
+					track = media.title;
+					duration = media.duration;
+
+					updateNowPlaying();
+
+					unixStart = Math.round(new Date().getTime() / 1000);
+					start = unsafeWindow.API.getTimeElapsed();
+					elapsed = 0;
+					scrobbled = false;
+					self.port.emit('scrobbled', false);
+
+					selfPlaying = 0;
+
+					if (unsafeWindow.API.getUser().id === unsafeWindow.API.getDJ().id) selfPlaying = 1;
+				}
+
+				if (!scrobbled && duration > 29) {
+					var elapsed = unsafeWindow.API.getTimeElapsed() - start;
+
+					if (elapsed >= Math.min(duration / 2, 240)) {
+						scrobbled = true;
+						self.port.emit('scrobbled', true);
+
+						var parameters = {
+							'sk': lastfmSession,
+							'artist': artist, 
+							'track': track, 
+							'duration': duration,
+							'timestamp': unixStart,
+							'chosenByUser': selfPlaying
+						};
+
+						call('track.scrobble', checkMedia, parameters);
+					}
+				}
+			}
+
+			else {
+				if (artist !== '' || track !== '' || duration !== 0) {
+					artist = '';
+					track = '';
+					duration = 0;
+
+					unixStart = 0;
+					start = 0;
+					elapsed = 0;
+					scrobbled = false;
+					self.port.emit('scrobbled', false);
+
+					selfPlaying = 0;
+				}
 			}
 		}
 
-		else {
-			artist = '';
-			track = '';
-			duration = 0;
+		checkMediaLoop = window.setTimeout(checkMedia, 2000);
+	}
+}
+
+function updateNowPlaying(response) {
+	if (response) {
+		console.log('Updated now playing: ', response);
+	}
+
+	else {
+		var parameters = {
+			'sk': lastfmSession,
+			'artist': artist, 
+			'track': track,
+			'duration': duration
 		}
+
+		call('track.updateNowPlaying', updateNowPlaying, parameters);
 	}
-
-	window.setTimeout(check, 2000);
-}
-
-function process() {
-	var parameters = {
-		'sk': lastfmSession,
-		'artist': artist, 
-		'track': track,
-		'duration': duration
-	}
-
-	call('track.updateNowPlaying', updatePlaying, parameters);
-}
-
-function updatePlaying(data) {
-	console.log('Success ', data);
 }
