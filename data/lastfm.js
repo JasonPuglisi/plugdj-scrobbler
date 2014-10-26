18f4b216793b6356cf1c025da61b7e9ab56d594f
Jason Puglisi <jason@puglisi.im>
2014-10-26 13:43:19 -0400

    Move globals to new file

--- data/lastfm.js
+++ data/lastfm.js
@@ -1,6 +1,3 @@
-var lastfmKey = '8c2c754cdff3f4da859781b4e229e807';
-var lastfmSecret = '180463126834e5dae185dbc5d5508b0f';
-
 var lastfmToken;
 var lastfmSession;
 
@@ -149,17 +146,4 @@ function getSession(response) {
 			error: getSession
 		});
 	}
-}
-
-
-function getSig(parameters) {
-	var sig = '';
-
-	Object.keys(parameters).sort().forEach(function(key) {
-		sig += key + parameters[key];
-	});
-
-	sig += lastfmSecret;
-
-	return CryptoJS.MD5(sig).toString(CryptoJS.enc.Hex);
 }
