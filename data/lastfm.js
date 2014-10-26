3ad7e5ce2eb064a68c60c756e5279a675c0e3133
Jason Puglisi <jason@puglisi.im>
2014-10-26 10:07:50 -0400

    Cleaned code, added saved session loading

--- data/lastfm.js
+++ data/lastfm.js
@@ -6,25 +6,55 @@ var lastfmSession;
 
 var authStage = 0;
 
-$('#lastfmAuthenticate').click(function() {
+$('.instructions').click(function() {
 	if (authStage === 0) {
-		authStage++;
+		self.port.emit('setSession', '');
+
 		lastfmAuthenticate();
 	}
 
 	else if (authStage === 1) {
-		authStage++;
 		lastfmConfirm();
 	}
 });
+
+self.port.on('setSession', lastfmConfirm);
+
+setStage('default');
+
+function setStage(stage) {
+	switch(stage) {
+		case 'default':
+			authStage = 0;
+			setInstructions('Your account is not linked. Click to complete the process, then come back here.');
+		break;
+		case 'getting_token':
+			authStage = 1;
+			setInstructions('Click to confirm your account link.');
+		break;
+		case 'link_completed':
+			authStage = 0;
+			setInstructions('Account linked. Click to relink.');
+		break;
+		case 'link_failed':
+			authStage = 0;
+			setInstructions('Account link failed. Click to try again.');
+		break;
+	}
+}
+
+function setInstructions(message) {
+	$('.instructions').html(message);
+}
 
 function lastfmAuthenticate(token) {
 	if (token) {
 		lastfmToken = token;
 
+		self.port.emit('setPanelDisplay', false);
 		self.port.emit('openTab', 'http://www.last.fm/api/auth/?api_key=' + lastfmKey + '&token=' + token);
 
-		$('#lastfmAuthenticate').html('Click to confirm your account link.');
+		setStage('getting_token');
 	}
 
 	else {
@@ -38,8 +68,7 @@ function lastfmConfirm(session) {
 
 		self.port.emit('setSession', session);
 
-		authStage = 0;
-		$('#lastfmAuthenticate').html('Account link confirmed. Click to relink.');
+		setStage('link_completed');
 	}
 
 	else {
@@ -65,8 +94,7 @@ function getToken(response) {
 function getSession(response) {
 	if (response) {
 		if (!response.session) {
-			authStage = 0;
-			$('#lastfmAuthenticate').html('Authentication failed. Click to try again.');
+			setStage('link_failed');
 		} else {
 			lastfmConfirm(response.session.key);
 		}
